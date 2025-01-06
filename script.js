c218dc87867fac0613cec95ff91d215f3290165b
Bradley Aboua <clubrocker13@gmail.com>
2025-01-06 11:44:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Possible choices
+const choices = ["rock", "paper", "scissors"];
+// Function to get the computer's random choice
+const getComputerChoice = () => {
+    // TODO: Generate a random index and return the corresponding choice
+};
+// Function to determine the winner
+const getWinner = (player, computer) => {
+    // TODO: Compare player and computer choices to determine the winner
+    // Return "It's a tie!", "You win!", or "Computer wins!"
+};
